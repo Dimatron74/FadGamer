8b7a2c9750d415762f1e56580af45a70df19378d
Dimatron74 <logachev_16@bk.ru>
2024-11-28 17:14:07 +0300

    Update models.py

--- website/apps/profiles/models.py
+++ website/apps/profiles/models.py
@@ -1,7 +1,35 @@
 import uuid
 from django.contrib.auth.hashers import make_password, check_password
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, nickname, password=None, **extra_fields):
+        if not nickname:
+            raise ValueError('The Nickname field must be set')
+        nickname = self.normalize_email(nickname)
+        user = self.model(nickname=nickname, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, nickname, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(nickname, password, **extra_fields)
+
+    def get_by_natural_key(self, nickname):
+        return self.get(nickname=nickname)
+
+
 
 class Role(models.Model):
     name = models.CharField(max_length=50, unique=True, primary_key=True)
@@ -13,11 +41,39 @@ class Role(models.Model):
     @classmethod
     def get_default_role(cls):
         return cls.objects.get_or_create(name='user')[0]
-
+    
 class Email(models.Model):
-    email = models.EmailField(unique=True, primary_key=True)
-    user = models.OneToOneField("User", on_delete=models.CASCADE, related_name='email')
-    binding_date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+class UserEmail(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+    is_confirmed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'email')
+
+    def __str__(self):
+        return f'{self.user.name} - {self.email.email}'
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    roles = models.ManyToManyField(Role, related_name='users', blank=True)
+    nickname = models.CharField(unique=True, max_length=255)
+    password = models.CharField(max_length=255)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    birth_date = models.DateTimeField(null=True)
+    phone_number = models.CharField(null=True, max_length=20)
+    is_blocked = models.BooleanField(default=False)
+    registration_date = models.DateTimeField(auto_now_add=True)
 
     NOTIFICATION_CHOICES = [
         ('all', 'All Notifications'),
@@ -26,24 +82,13 @@ class Email(models.Model):
     ]
     notification_type = models.CharField(max_length=50, choices=NOTIFICATION_CHOICES, default='all')
 
-    def __str__(self):
-        return self.email
-
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    roles = models.ManyToManyField(Role, related_name='users', blank=True)
-    nickname = models.CharField(unique=True, max_length=255)
-    email = models.OneToOneField(Email, on_delete=models.CASCADE, related_name='user')
-    password = models.CharField(max_length=255)
-    avatar = models.BinaryField(null=True)
-    birth_date = models.DateTimeField(null=True)
-    phone_number = models.CharField(null=True, max_length=20)
-    is_blocked = models.BooleanField(default=False)
+    objects = UserManager()
 
     USERNAME_FIELD = 'nickname'
     REQUIRED_FIELDS = []
+    
+    def get_current_email(self):
+        return self.emails.filter(is_active=True).latest('binding_date')
 
     def set_password(self, password):
         self.password = make_password(password)
@@ -52,7 +97,10 @@ class User(AbstractBaseUser, PermissionsMixin):
         return check_password(password, self.password)
 
     def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # Сначала сохраняем объект self в базе данных
+        using = kwargs.get('using') or 'postgres'
+        print(using)
+        default_role = Role.objects.using(using).get_or_create(name='user')[0]
         if not self.roles.exists():
-            self.roles.add(Role.get_default_role())
-        super().save(*args, **kwargs)
+            self.roles.add(default_role)
 
