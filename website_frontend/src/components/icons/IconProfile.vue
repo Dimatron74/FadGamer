128090d2bb1da1679fdb3a2f4a9a8bce93613f08
Dimatron74 <logachev_16@bk.ru>
2024-12-15 14:28:45 +0300

    Create IconProfile.vue

---
+++ website_frontend/src/components/icons/IconProfile.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg width="40.000000" height="40.000000" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <desc>
+        Created with Pixso.
+    </desc>
+    <defs>
+      <clipPath id="clip20_47">
+        <rect id="9025024_user_circle_plus_light_icon 1" rx="0.000000" width="39.000000" height="39.000000" transform="translate(0.500000 0.500000)" fill="white" fill-opacity="0"/>
+      </clipPath>
+    </defs>
+    <rect id="9025024_user_circle_plus_light_icon 1" rx="0.000000" width="39.000000" height="39.000000" transform="translate(0.500000 0.500000)" fill="#E6E6E6" fill-opacity="0"/>
+    <g clip-path="url(#clip20_47)">
+      <path id="Vector" d="M20 25C16.54 25 13.75 22.2 13.75 18.75C13.75 15.29 16.54 12.5 20 12.5C23.45 12.5 26.25 15.29 26.25 18.75C26.25 22.2 23.45 25 20 25Z" stroke="#E6E6E6" stroke-opacity="1.000000" stroke-width="2.000000" stroke-linejoin="round"/>
+      <path id="Vector" d="M9.96 31.15C10.9 29.3 12.34 27.74 14.11 26.66C15.88 25.57 17.92 24.99 20 24.99C22.07 24.99 24.11 25.57 25.88 26.66C27.65 27.74 29.09 29.3 30.03 31.15" stroke="#E6E6E6" stroke-opacity="1.000000" stroke-width="2.000000" stroke-linejoin="round" stroke-linecap="round"/>
+      <path id="Vector" d="M27.5 8.75L35 8.75" stroke="#E6E6E6" stroke-opacity="1.000000" stroke-width="2.000000" stroke-linejoin="round" stroke-linecap="round"/>
+      <path id="Vector" d="M31.25 5L31.25 12.5" stroke="#E6E6E6" stroke-opacity="1.000000" stroke-width="2.000000" stroke-linejoin="round" stroke-linecap="round"/>
+      <path id="Vector" d="M34.81 17.64C34.93 18.42 35 19.2 35 20C35 22.96 34.12 25.86 32.47 28.33C30.82 30.8 28.48 32.72 25.74 33.85C22.99 34.99 19.98 35.29 17.07 34.71C14.16 34.13 11.49 32.7 9.39 30.6C7.29 28.5 5.86 25.83 5.28 22.92C4.7 20.01 5 17 6.14 14.25C7.27 11.51 9.19 9.17 11.66 7.52C14.13 5.87 17.03 5 20 5C20.79 4.99 21.57 5.06 22.35 5.18" stroke="#E6E6E6" stroke-opacity="1.000000" stroke-width="2.000000" stroke-linejoin="round" stroke-linecap="round"/>
+    </g>
+  </svg>
+</template>
+
