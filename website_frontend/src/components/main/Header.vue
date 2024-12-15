7718de38752f6c4b582b565488f184a31f31b404
Dimatron74 <logachev_16@bk.ru>
2024-12-15 14:28:49 +0300

    Update Header.vue

--- website_frontend/src/components/main/Header.vue
+++ website_frontend/src/components/main/Header.vue
@@ -6,7 +6,7 @@
           <div class="text-2xl w-48 text-center">FAD GAMERS</div>
         </RouterLink>
       </div>
-      <nav class="flex items-center text-lg justify-between ml-[74px] tracking-wider gap-2 tablet-max:gap-5 desktop-min:gap-10">
+      <nav class="items-center text-lg justify-between ml-[74px] tracking-wider gap-2 hidden tablet-max:flex desktop-min:gap-10">
         <RouterLink to="/" class="hover:text-mywhite-5">ГЛАВНАЯ</RouterLink>
         <RouterLink to="/about" class="hover:text-mywhite-5">ИГРЫ</RouterLink>
         <RouterLink to="/about" class="hover:text-mywhite-5">НОВОСТИ</RouterLink>
@@ -20,33 +20,33 @@
           </div>
         </div>
       </nav>
-      <div class="flex items-center justify-end gap-10 ml-auto">
-        <RouterLink v-if="!userStore.user.access" to="/signup" class="hover:text-mywhite-5">Зарегистироваться</RouterLink>
-        <RouterLink v-if="!userStore.user.access" to="/login" class="hover:text-mywhite-5">Авторизоваться</RouterLink>
+      <div class="ml-auto">
+        <RouterLink v-if="!userStore.user.access" to="/login" class="hover:text-mywhite-5 flex items-center gap-2">
+          <span>ВОЙТИ</span>
+          <IconProfile />
+        </RouterLink>
         <RouterLink v-else to="/profile" class="hover:text-mywhite-5">ПРОФИЛЬ</RouterLink>
       </div>
     </div>
   </header>
 </template>
 
-<script>
-export default {
-  props: {
-    userStore: {
-      type: Object,
-      required: true
-    }
-  },
-  data() {
-    return {
-      dropdownOpen: false
-    };
-  },
-  methods: {
-    toggleDropdown() {
-      this.dropdownOpen = !this.dropdownOpen;
-    }
+<script setup>
+import { ref } from 'vue';
+import IconProfile from "@/components/icons/IconProfile.vue";
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  userStore: {
+    type: Object,
+    required: true
   }
+});
+
+const dropdownOpen = ref(false);
+
+function toggleDropdown() {
+  dropdownOpen.value = !dropdownOpen.value;
 }
 </script>
 
