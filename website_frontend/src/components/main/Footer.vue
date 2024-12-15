b794e9a0ad85461368e7a9e49f1ad71152c1cb62
Dimatron74 <logachev_16@bk.ru>
2024-12-15 16:44:39 +0300

    update footer and header

--- website_frontend/src/components/main/Footer.vue
+++ website_frontend/src/components/main/Footer.vue
@@ -1,28 +1,70 @@
 <template>
-  <footer class="fixed bottom-0 left-0 right-0 bg-black text-white p-4">
-    <div class="container mx-auto">
-      <div class="flex justify-between items-center">
-        <div class="text-sm">
-          &copy; 2024 FadGamers. All rights reserved.
-        </div>
-        <nav class="space-x-4">
-          <RouterLink to="/about" class="hover:text-gray-400">О нас</RouterLink>
-          <RouterLink to="/" class="hover:text-gray-400">Контакты</RouterLink>
-          <RouterLink to="/" class="hover:text-gray-400">Политика конфиденциальности</RouterLink>
-        </nav>
-      </div>
-    </div>
+  <footer class="bg-myblack-2 py-6 h-60">
+    <nav class="container mx-auto flex flex-col justify-center items-center text-mywhite-1">
+      <ul class="flex items-center justify-center gap-5 text-sm pb-5">
+        <li>
+          <RouterLink to="/about" class="hover:text-mywhite-5" @click="scrollToTop">О компании</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/" class="">Сотрудничество</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/" class="">Политика конфиденциальности</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/" class="">Лицензионное соглашение</RouterLink>
+        </li>
+      </ul>
+      <ul class="flex items-center justify-center gap-5 pb-[30px]">
+        <li>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <IconTelegram />
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <IconVK />
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <IconYouTube />
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <IconDiscord />
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <IconX />
+          </a>
+        </li>
+      </ul>
+      <div class="text-3xl text-center pb-3" style="font-family: 'Roboto', sans-serif; font-weight: bold;">FAD GAMERS</div>
+      <div class="mt-auto text-base text-gray-500">&copy; 2024 FadGamers. Все права защищены.</div>
+
+    </nav>
   </footer>
 </template>
 
+<script setup>
+import { defineProps } from 'vue';
+import IconVK from "@/components/icons/IconVK.vue"
+import IconDiscord from "@/components/icons/IconDiscord.vue"
+import IconTelegram from "@/components/icons/IconTelegram.vue"
+import IconX from "@/components/icons/IconX.vue"
+import IconYouTube from "@/components/icons/IconYouTube.vue"
 
-<script>
-export default {
-  props: {
-    userStore: {
-      type: Object,
-      required: true
-    }
+const props = defineProps({
+  userStore: {
+    type: Object,
+    required: true
   }
+});
+
+function scrollToTop() {
+  window.scrollTo(0,0);
 }
 </script>
--- website_frontend/src/components/main/Header.vue
+++ website_frontend/src/components/main/Header.vue
@@ -1,31 +1,31 @@
 <template>
-  <header class="bg-myblack-2 text-mywhite-3 body-font" style="font-family: 'Roboto', sans-serif; font-weight: bold;">
+  <header class="bg-myblack-2 text-mywhite-3 body-font sticky" style="font-family: 'Roboto', sans-serif; font-weight: bold;">
     <div class="flex p-6 flex-col md:flex-row items-center h-20">
       <div class="flex items-center justify-start">
-        <RouterLink to="/">
+        <RouterLink to="/" @click="scrollToTop">
           <div class="text-2xl w-48 text-center">FAD GAMERS</div>
         </RouterLink>
       </div>
       <nav class="items-center text-lg justify-between ml-[74px] tracking-wider gap-2 hidden tablet-max:flex desktop-min:gap-10">
-        <RouterLink to="/" class="hover:text-mywhite-5">ГЛАВНАЯ</RouterLink>
-        <RouterLink to="/about" class="hover:text-mywhite-5">ИГРЫ</RouterLink>
-        <RouterLink to="/about" class="hover:text-mywhite-5">НОВОСТИ</RouterLink>
-        <RouterLink to="/about" class="hover:text-mywhite-5">ЦЕНТР ПОДДЕРЖКИ</RouterLink>
+        <RouterLink to="/" class="hover:text-mywhite-5" @click="scrollToTop">ГЛАВНАЯ</RouterLink>
+        <RouterLink to="/about" class="hover:text-mywhite-5" @click="scrollToTop">ИГРЫ</RouterLink>
+        <RouterLink to="/about" class="hover:text-mywhite-5" @click="scrollToTop">НОВОСТИ</RouterLink>
+        <RouterLink to="/about" class="hover:text-mywhite-5" @click="scrollToTop">ЦЕНТР ПОДДЕРЖКИ</RouterLink>
         <div @click="toggleDropdown" class="relative z-10">
           <span class="cursor-pointer hover:text-mywhite-5">ДРУГОЕ</span>
           <div v-if="dropdownOpen" class="absolute bg-myblack-2 shadow-lg mt-2">
-            <RouterLink to="/promo" class="block px-4 py-2 text-mywhite-3 hover:bg-myblack-3">Активировать промокод</RouterLink>
-            <RouterLink to="" class="block px-4 py-2 text-mywhite-3 hover:bg-myblack-3">О компании</RouterLink>
-            <RouterLink to="" class="block px-4 py-2 text-mywhite-3 hover:bg-myblack-3">Сотрудничество</RouterLink>
+            <RouterLink to="/promo" class="block px-4 py-2 text-mywhite-3 hover:bg-myblack-3" @click="scrollToTop">Активировать промокод</RouterLink>
+            <RouterLink to="/about" class="block px-4 py-2 text-mywhite-3 hover:bg-myblack-3" @click="scrollToTop">О компании</RouterLink>
+            <RouterLink to="" class="block px-4 py-2 text-mywhite-3 hover:bg-myblack-3" @click="scrollToTop">Сотрудничество</RouterLink>
           </div>
         </div>
       </nav>
       <div class="ml-auto">
-        <RouterLink v-if="!userStore.user.access" to="/login" class="hover:text-mywhite-5 flex items-center gap-2">
+        <RouterLink v-if="!userStore.user.access" to="/login" class="hover:text-mywhite-5 flex items-center gap-2" @click="scrollToTop">
           <span>ВОЙТИ</span>
           <IconProfile />
         </RouterLink>
-        <RouterLink v-else to="/profile" class="hover:text-mywhite-5">ПРОФИЛЬ</RouterLink>
+        <RouterLink v-else to="/profile" class="hover:text-mywhite-5" @click="scrollToTop">ПРОФИЛЬ</RouterLink>
       </div>
     </div>
   </header>
@@ -48,5 +48,18 @@ const dropdownOpen = ref(false);
 function toggleDropdown() {
   dropdownOpen.value = !dropdownOpen.value;
 }
+
+function scrollToTop() {
+  window.scrollTo(0,0);
+}
 </script>
+
+<style scoped>
+.sticky {
+  position: fixed;
+  top: 0;
+  z-index: 1000;
+  width: 100%;
+}
+</style>
 
