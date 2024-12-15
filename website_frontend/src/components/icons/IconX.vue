fbd5aefdd5ba9c9259e6500bad265c3890972323
Dimatron74 <logachev_16@bk.ru>
2024-12-15 16:44:07 +0300

    create icons

---
+++ website_frontend/src/components/icons/IconX.vue
@@ -0,0 +1,23 @@
+<template>
+	<svg width="39.000000" height="40.000000" viewBox="0 0 39 40" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+		<desc>
+				Created with Pixso.
+		</desc>
+		<defs>
+			<clipPath id="clip25_242">
+				<rect id="11244080_x_twitter_elon musk_twitter new logo_icon 1" rx="0.000000" width="38.000000" height="39.000000" transform="translate(0.500000 0.500000)" fill="white" fill-opacity="0"/>
+			</clipPath>
+		</defs>
+		<rect id="11244080_x_twitter_elon musk_twitter new logo_icon 1" rx="0.000000" width="38.000000" height="39.000000" transform="translate(0.500000 0.500000)" fill="#FFFFFF" fill-opacity="0"/>
+		<g clip-path="url(#clip25_242)">
+			<mask id="mask25_245" mask-type="alpha" maskUnits="userSpaceOnUse" x="0.000000" y="0.000000" width="39.000000" height="40.000000">
+				<path id="Vector" d="M0 0L39 0L39 40L0 40L0 0Z" fill="#000000" fill-opacity="1.000000" fill-rule="evenodd"/>
+			</mask>
+			<g mask="url(#mask25_245)">
+				<path id="Vector" d="M22.25 17.7L33.05 5L30.49 5L21.12 16.02L13.63 5L5 5L16.32 21.67L5 35L7.55 35L17.45 23.35L25.36 35L34 35L22.25 17.7L22.25 17.7ZM18.75 21.82L17.6 20.16L8.48 6.94L12.4 6.94L19.77 17.61L20.92 19.27L30.49 33.13L26.56 33.13L18.75 21.82L18.75 21.82Z" fill="#000000" fill-opacity="0" fill-rule="nonzero"/>
+				<path id="Vector" d="M33.05 5L30.49 5L23.81 12.86L22.45 14.45L22.25 14.69L21.12 16.02L19.92 14.26L19.54 13.7L18.79 12.59L13.63 5L5 5L7.71 9L8.48 10.12L9.03 10.94L15.12 19.91L16.32 21.67L16.19 21.82L14.92 23.32L14.92 23.32L7.55 31.98L6.07 33.73L5 35L7.55 35L14.77 26.51L17.45 23.35L18.65 25.11L19.78 26.78L25.36 35L34 35L31.28 31L30.49 29.84L30.02 29.13L24.58 21.13L23.45 19.46L22.25 17.7L22.25 17.7L22.25 17.7L23.6 16.11L30.49 8.01L31.98 6.26L33.05 5ZM13.63 8.72L17.07 13.7L19.77 17.61L20.92 19.27L21.41 19.98L22.12 21.02L22.25 21.2L22.59 21.7L22.68 21.82L27.73 29.13L29.02 31L29.22 31.3L30.49 33.13L26.56 33.13L25.36 31.39L19.96 23.57L18.75 21.82L18.75 21.82L17.6 20.16L16.4 18.41L11.24 10.94L9.89 9L9.74 8.78L8.48 6.94L12.4 6.94L13.63 8.72Z" fill="#CCCCCC" fill-opacity="1.000000" fill-rule="evenodd"/>
+			</g>
+		</g>
+	</svg>
+</template>
+
