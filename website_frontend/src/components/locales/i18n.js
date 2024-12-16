bc5eec7d088ba9632a0144c4161e0dd80f98058d
Dimatron74 <logachev_16@bk.ru>
2024-12-16 19:17:33 +0300

    update translate

--- website_frontend/src/components/locales/i18n.js
+++ website_frontend/src/components/locales/i18n.js
@@ -1,8 +1,10 @@
 import { createI18n } from 'vue-i18n';
 import en from './en.json';
 import ru from './ru.json';
+import cn from './zh-CN.json';
+import hi from './hi.json';
 
-const messages = { en, ru };
+const messages = { en, ru, cn, hi };
 
 const i18n = createI18n({
   locale: 'ru', // язык по умолчанию
