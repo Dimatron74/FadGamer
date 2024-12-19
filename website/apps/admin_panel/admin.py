aa3c45fd4488fd6894b8b119d3f129b3d3a5b5e9
Dimatron74 <logachev_16@bk.ru>
2024-12-19 16:10:58 +0300

    Update admin.py

--- website/apps/admin_panel/admin.py
+++ website/apps/admin_panel/admin.py
@@ -1,3 +1,9 @@
+# website/apps/support/admin.py
 from django.contrib import admin
+from apps.support.models import SupportRequest
 
-# Register your models here.
+# class SupportRequestAdmin(admin.ModelAdmin):
+#     list_display = ('question', 'text', 'created_at', 'answered')
+#     search_fields = ('question',)
+
+# admin.site.register(SupportRequest, SupportRequestAdmin)
