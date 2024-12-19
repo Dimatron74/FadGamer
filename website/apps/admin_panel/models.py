a40c4a8701ffba90f1f00a0d247722ecbb94b219
Dimatron74 <logachev_16@bk.ru>
2024-12-19 16:12:44 +0300

    try SupportPage

--- website/apps/admin_panel/models.py
+++ website/apps/admin_panel/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from wagtail.admin.panels import FieldPanel
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from apps.support.models import SupportRequest
 
-# Create your models here.
+# class SupportPage(Page):
+#     body = RichTextField(blank=True)
+
+#     content_panels = Page.content_panels + [
+#         FieldPanel('body', classname="full"),
+#     ]
+
+#     def get_context(self, request):
+#         context = super().get_context(request)
+#         context['support_requests'] = SupportRequest.objects.all()
+#         return context
