8e3a7c23dc2a7f1805d66fbdcc9dcab734d9d9f6
Dimatron74 <logachev_16@bk.ru>
2025-03-10 16:22:39 +0300

    Update settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -198,13 +198,13 @@ STATICFILES_FINDERS = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5174",
-    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:5174",
-    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 
 CORS_ALLOW_METHODS = [
