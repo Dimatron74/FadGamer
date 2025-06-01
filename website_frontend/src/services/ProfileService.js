967b3b0f8b0e516e9c07270d3cf95bc38aaeb45d
Dimatron74 <logachev_16@bk.ru>
2025-06-01 16:12:05 +0300

    Create ProfileService.js
    Забыл про профиль сервис...

---
+++ website_frontend/src/services/ProfileService.js
@@ -0,0 +1,46 @@
+// src/services/profileService.js
+
+import axios from 'axios'
+import { useUserStore } from '@/stores/user'
+
+const apiClient = axios.create({
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  }
+})
+
+// Перехватчик для добавления токена
+apiClient.interceptors.request.use(async config => {
+  const userStore = useUserStore()
+
+  // Если нет access-токена — пытаемся обновить его
+  if (!userStore.user.access) {
+    try {
+      await userStore.refreshToken()
+    } catch (e) {
+      console.error('Не удалось обновить токен')
+      return Promise.reject(e)
+    }
+  }
+
+  // Добавляем заголовок авторизации
+  config.headers.Authorization = `Bearer ${userStore.user.access}`
+  return config
+})
+
+export default {
+  getProfile() {
+    return apiClient.get('/profiles/me/')
+  },
+  updateProfile(data) {
+    return apiClient.patch('/profiles/profile/', data)
+  },
+  uploadAvatar(formData) {
+    return apiClient.post('/profiles/avatar/upload/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+  }
+}
