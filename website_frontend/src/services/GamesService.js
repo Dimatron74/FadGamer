feeb0da38a17f95ee1b3b3fa6df7a8a6064ade0d
Dimatron74 <logachev_16@bk.ru>
2025-06-03 15:34:11 +0300

    Update GamesService.js

--- website_frontend/src/services/GamesService.js
+++ website_frontend/src/services/GamesService.js
@@ -9,22 +9,7 @@ const apiClient = axios.create({
   }
 })
 
-// Перехватчик для добавления токена
-apiClient.interceptors.request.use(async config => {
-  const userStore = useUserStore()
-  // Если нет access-токена — пытаемся обновить его
-  if (!userStore.user.access) {
-    try {
-      await userStore.refreshToken()
-    } catch (e) {
-      console.error('Не удалось обновить токен')
-      return Promise.reject(e)
-    }
-  }
-  // Добавляем заголовок авторизации
-  config.headers.Authorization = `Bearer ${userStore.user.access}`
-  return config
-})
+
 
 export default {
   getGames() {
